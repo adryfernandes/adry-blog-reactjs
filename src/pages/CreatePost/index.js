570228d1dbd3f8543adc1f75581031753bcac87d
adryfernandes <adryane.fernandes.dev@gmail.com>
2023-04-05 20:09:49 -0300

    Texto e inserção de imagens funcionando

--- src/pages/CreatePost/index.js
+++ src/pages/CreatePost/index.js
@@ -1,15 +1,13 @@
-import React, { useState, useRef } from "react";
+import { useState, useRef } from "react";
 import ReactQuill from "react-quill";
-import Dropzone, { useDropzone } from "react-dropzone";
 import "react-quill/dist/quill.snow.css";
-import { DropzoneContainer } from "./styled";
 
 export function CreatePost() {
   const [content, setContent] = useState("");
   const quillRef = useRef();
 
-  const handleDrop = (files) => {
-    const file = files[0];
+  const handleImageUpload = (e) => {
+    const file = e.target.files[0];
     const reader = new FileReader();
     reader.onload = () => {
       let index = 0;
@@ -24,27 +22,10 @@ export function CreatePost() {
     reader.readAsDataURL(file);
   };
 
-  const { getRootProps, getInputProps, isDragActive } = useDropzone();
-
   return (
     <div>
-      <DropzoneContainer
-        onDrop={handleDrop}
-        {...getRootProps()}
-        isDragActive={isDragActive}>
-        <input {...getInputProps()} />
-        {isDragActive ? (
-          <p>Arraste e solte as imagens aqui ou clique para selecionar</p>
-        ) : (
-          <p>Arquivo selecionado</p>
-        )}
-      </DropzoneContainer>
-      <ReactQuill
-        ref={quillRef}
-        value={content}
-        onChange={setContent}
-        style={{ height: "50vh" }}
-      />
+      <input type="file" onChange={handleImageUpload} />
+      <ReactQuill ref={quillRef} value={content} onChange={setContent} />
     </div>
   );
 }
